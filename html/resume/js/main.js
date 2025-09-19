f282f2aa1877a8a65d0fcae657f61b730a310884
Yulin Li <yulinlifight@163.com>
2025-09-20 00:55:14 +0800

    Site updated: 2025-09-20 00:55:13

--- html/resume/js/main.js
+++ html/resume/js/main.js
@@ -1,2 +1,9 @@
 // 页面通用逻辑，可用于后续扩展
 console.log("页面加载完成");
+
+const sidebar = document.getElementById('sidebar');
+const toggleBtn = document.getElementById('sidebar-toggle');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+});
